2cc5a2d4280ef19f20bd6c463bc010a483241c97
sim-mcqueen <s.mcqueen@digipen.edu>
2022-03-02 13:59:59 -0800

    Done platforms

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     public float speed;
+    private Vector3 offset;
+    public float minWidth;
+    public float maxWidth;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.localScale = new Vector3(Random.Range(minWidth, maxWidth), 1);
+        offset = new Vector3(-speed / 500, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += offset;
     }
 }
--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -9,6 +9,8 @@ public class PlatformSpawner : MonoBehaviour
     public float lowerBound;
     public float leftPos;
     public GameObject platform;
+    public int minTimeToWait;
+    public int maxTimeToWait;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +23,7 @@ public class PlatformSpawner : MonoBehaviour
 
     IEnumerator SpawnPlatform()
     {
-        yield return new WaitForSeconds(Random.Range(1 ,5));
+        yield return new WaitForSeconds(Random.Range(minTimeToWait, maxTimeToWait));
         Instantiate(platform, new Vector3(leftPos, Random.Range(lowerBound, upperBound)), Quaternion.identity);
         StartCoroutine(SpawnPlatform());
     }
