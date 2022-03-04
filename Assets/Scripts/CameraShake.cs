c4758bed0abe3b99314bf1d47dcebb3f79567dce
andrew-dahlmanoeth <andrew.dahlmanoeth@digipen.edu>
2022-03-04 14:08:53 -0800

    Fixed end scene and added non working camera shake

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private float timeElapsed;
+    private float xOffset;
+    private float yOffset;
+    private Vector3 startPos;
+    private ObstacleHitEvent obstacleHitEvent;
+
+    private void Awake()
+    {
+        obstacleHitEvent = FindObjectOfType<ObstacleHitEvent>();
+    }
+
+    private void Start()
+    {
+        startPos = transform.position;
+        obstacleHitEvent.OnHitObstacle += ObstacleHitEvent_OnHitObstacle;
+    }
+
+    private void ObstacleHitEvent_OnHitObstacle(object sender, System.EventArgs e)
+    {
+        ShakeScreen(.5f, 2);
+    }
+
+    IEnumerator ShakeScreen(float duration, float mag)
+    {
+        timeElapsed = 0f;
+
+        while(timeElapsed < duration)
+        {
+            xOffset = Random.Range(-0.5f, 0.5f) * mag;
+            yOffset = Random.Range(-0.5f, 0.5f) * mag;
+
+            transform.position = new Vector3(xOffset, yOffset, startPos.z);
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = startPos;
+    }
+}
