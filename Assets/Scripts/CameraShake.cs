f625cc749f48628475facd4110d65b856079c280
andrew-dahlmanoeth <andrew.dahlmanoeth@digipen.edu>
2022-03-08 14:39:58 -0800

    Added new random event, postprocessing, etc.

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -7,21 +7,31 @@ public class CameraShake : MonoBehaviour
 
     public float hitMag;
     public float hitDuration;
+    public float rotMag;
+    public float rotDuration;
     private float timeElapsed;
     private float xOffset;
     private float yOffset;
     private Vector3 startPos;
     private ObstacleHitEvent obstacleHitEvent;
+    private RotateCameraEvent rotateCameraEvent;
 
     private void Awake()
     {
+        rotateCameraEvent = FindObjectOfType<RotateCameraEvent>();
         obstacleHitEvent = FindObjectOfType<ObstacleHitEvent>();
     }
 
     private void Start()
     {
         startPos = transform.position;
+        rotateCameraEvent.OnRotateCamera += RotateCameraEvent_OnRotateCamera;
         obstacleHitEvent.OnHitObstacle += ObstacleHitEvent_OnHitObstacle;
+    }
+
+    private void RotateCameraEvent_OnRotateCamera(object sender, System.EventArgs e)
+    {
+        StartCoroutine(ShakeScreen(rotDuration, rotMag));
     }
 
     private void ObstacleHitEvent_OnHitObstacle(object sender, System.EventArgs e)
