fae09cc477de7d592c20c407d952b678d5c0d1fa
andrew-dahlmanoeth <andrew.dahlmanoeth@digipen.edu>
2022-03-10 14:08:54 -0800

    Added background slowdown on hit

--- Assets/Scripts/SideScroll.cs
+++ Assets/Scripts/SideScroll.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 public class SideScroll : MonoBehaviour
 {
     public float scrollSpeed;
+    public float slowTime;
+    private float startScrollSpeed;
+    public float endScrollSpeed;
     private Vector3 startPosition;
     private Material material;
     private Vector2 offset;
     private DeathEvent DE;
+    private ObstacleHitEvent obstacleHitEvent;
+    private bool dead = false;
 
     private void Awake()
     {
+        obstacleHitEvent = FindObjectOfType<ObstacleHitEvent>();
         DE = FindObjectOfType<DeathEvent>();
         material = GetComponent<Renderer>().material;
     }
     
 
+    private void ObstacleHitEvent_OnHitObstacle(object sender, System.EventArgs e)
+    {
+        StartCoroutine(SlowScroll());
+    }
+
     private void DE_OnDeath(object sender, System.EventArgs e)
     {
+        dead = true;
         offset = new Vector2(0, 0);
     }
 
     private void Start()
     {
         // Canvas is invisible otherwise
+        startScrollSpeed = scrollSpeed;
+        obstacleHitEvent.OnHitObstacle += ObstacleHitEvent_OnHitObstacle;
         transform.position = new Vector3(0, 0, 100);
         startPosition = transform.position;
         offset = new Vector2(scrollSpeed, 0);
@@ -34,6 +48,46 @@ public class SideScroll : MonoBehaviour
     void FixedUpdate()
     {
         material.mainTextureOffset += offset * Time.deltaTime;
+    }
+
+    IEnumerator SlowScroll()
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < (slowTime))
+        {
+            if(!dead)
+            {
+                scrollSpeed = Mathf.Lerp(startScrollSpeed, endScrollSpeed, elapsedTime / slowTime);
+                offset = new Vector2(scrollSpeed, 0);
+                elapsedTime += Time.deltaTime;
+
+            }
+
+            yield return null;
+        }
+
+
+        StartCoroutine(SpeedScroll());
+    }
+
+    IEnumerator SpeedScroll()
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < (slowTime))
+        {
+            if(!dead)
+            {
+                scrollSpeed = Mathf.Lerp(endScrollSpeed, startScrollSpeed, elapsedTime / slowTime);
+                offset = new Vector2(scrollSpeed, 0);
+                elapsedTime += Time.deltaTime;
+
+            }
+
+
+            yield return null;
+        }
     }
 
 }
